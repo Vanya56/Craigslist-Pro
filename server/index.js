0875b84ae5633d085f79d3c99eb3e3ac552a65a0
Vanya Levchenko <ivanlevchenko56@gmail.com>
2019-08-08 23:51:34 -0600

    api created with title, price, and images from craigslist.

--- server/index.js
+++ server/index.js
@@ -2,13 +2,40 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const fetch = require('node-fetch');
-const cherio = require('cherio');
+const cheerio = require('cheerio');
 
 const app = express();
 
 app.use(cors());
 
 app.use(morgan('tiny'));
+
+function getResults(body) {
+    const $ = cheerio.load(body);
+    const rows = $('li.result-row');
+    const results = [];
+
+    rows.each((index, element) => {
+        const result = $(element);
+        const title = result.find('.result-title').text();
+        const price = $(result.find('.result-price').get(0)).text();
+        const imageData = result.find('a.result-image').attr('data-ids');
+        let images = [];
+        if (imageData) {
+            const parts = imageData.split(',');
+            images = parts.map((id) => {
+                return `https://images.craigslist.org/${id.split(':')[1]}_300x300.jpg`;
+            });
+        
+        }
+        results.push({
+            title,
+            price,
+            images
+        });
+    });
+    return results;
+}
 
 app.get('/', (request, response) => {
     response.json({
@@ -22,13 +49,13 @@ app.get('/search/:location/:search_term', (request, response) => {
     const search_term = request.params.search_term;
 
     const url = `https://${location}.craigslist.org/search/sss?query=${search_term}`
-// Gets the html text form the page
+    // Gets the html text form the page
     fetch(url)
         .then(response => response.text())
         .then(body => {
+            const results = getResults(body);
             response.json({
-                results: [],
-                body
+                results
             });
         });
 });
--- server/scratch.js
+++ server/scratch.js
@@ -2,7 +2,17 @@ var results = $('li.result-row');
 
 results.each((index, element) => {
     const result = $(element);
-    const title = result.find('.result-title').text();
-    console.log(title);
+    const imageData = result.find('a.result-image').attr('data-ids');
+    // const firstImage = result.find('div.swipe-wrap div[data-index = "0"] img').attr('src');
+    console.log(imageData);
+    if(imageData) {
+        const parts = imageData.split(',');
+        const ids = parts.map((id) => {
+            return `https://images.craigslist.org/${id.split(':')[1]}_300x300.jpg`;
+        });
+        console.log(ids);
+        
+    }
+    
     
 });
